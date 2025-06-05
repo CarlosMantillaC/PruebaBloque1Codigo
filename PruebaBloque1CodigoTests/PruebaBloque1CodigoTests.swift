a5af2e123874f7b147c6492b7a81e8fd94dda535
Carlos Mantilla <carlosmcot@gmail.com>
2025-06-05 05:01:11 -0500

    crea proyecto con estructura base

---
+++ PruebaBloque1CodigoTests/PruebaBloque1CodigoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaBloque1CodigoTests.swift
+//  PruebaBloque1CodigoTests
+//
+//  Created by Gopenux on 5/06/25.
+//
+
+import Testing
+@testable import PruebaBloque1Codigo
+
+struct PruebaBloque1CodigoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
