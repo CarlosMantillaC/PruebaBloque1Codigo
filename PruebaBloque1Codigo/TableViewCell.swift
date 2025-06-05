0f446e23e72ec6b003afafb2036a8b5e16417d23
Carlos Mantilla <carlosmcot@gmail.com>
2025-06-05 06:04:36 -0500

    agrega diseño sin funcionalidad de botones de reproducir y pausar en TableViewCell

--- PruebaBloque1Codigo/TableViewCell.swift
+++ PruebaBloque1Codigo/TableViewCell.swift
@@ -18,16 +18,41 @@ class TableViewCell: UITableViewCell {
         return label
     }()
     
+    private let playButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("▶️", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let pauseButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("⏸️", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        addSubview(nameLabel)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(playButton)
+        contentView.addSubview(pauseButton)
         
         NSLayoutConstraint.activate([
-            nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
-            nameLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -24),
+            
+            pauseButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -24),
+            pauseButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            pauseButton.widthAnchor.constraint(equalToConstant: 40),
+            
+            playButton.trailingAnchor.constraint(equalTo: pauseButton.leadingAnchor, constant: -10),
+            playButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            playButton.widthAnchor.constraint(equalToConstant: 40),
         ])
-
     }
     
     required init?(coder: NSCoder) {
